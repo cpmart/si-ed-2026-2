e2de740fb5984c7beaa972ea9cea01f51dbd000b
Cristiano Pires Martins <cpmart@gmail.com>
2026-08-21 20:17:31 -0300

    ver qual é o maior sem tratar valores iguais

---
+++ ex-02-maior-3v.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite o n1: '))
+n2 = int(input('Digite o n2: '))
+n3 = int(input('Digite o n3: '))
+
+if n1 > n2 and n1 > n3:
+    print('n1 é maior')
+if n2 > n1 and n2 > n3:
+    print('n2 é maior')
+if n3 > n1 and n3 > n2:
+    print('n3 é maior')
