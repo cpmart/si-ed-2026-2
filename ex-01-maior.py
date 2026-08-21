9383f7c624098b7144c3de9ddc1e8b627436db8b
Cristiano Pires Martins <cpmart@gmail.com>
2026-08-21 20:07:44 -0300

    programa completo

--- ex-01-maior.py
+++ ex-01-maior.py
@@ -1,2 +1,9 @@
 n1 = int(input('Digite o N1: '))
 n2 = int(input('Digite o N2: '))
+
+if n1 > n2:
+    print('N1 é maior que N2')
+elif n2 > n1:
+    print('N2 é maior que N1')
+else:
+    print('N2 é igual ao N1')
