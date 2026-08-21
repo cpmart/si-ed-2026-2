ff94b46ae47c1d9932f396bcdd1d8b6b64a592a5
Cristiano Pires Martins <cpmart@gmail.com>
2026-08-21 20:10:42 -0300

    troquei os nomes das variáveis

--- ex-01-maior.py
+++ ex-01-maior.py
@@ -1,9 +1,8 @@
-n1 = int(input('Digite o N1: '))
-n2 = int(input('Digite o N2: '))
-
-if n1 > n2:
-    print('N1 é maior que N2')
-elif n2 > n1:
-    print('N2 é maior que N1')
+numero1 = int(input('Digite o numero1: '))
+numero2 = int(input('Digite o numero2: '))
+if numero1 > numero2:
+    print('numero1 é maior que numero2')
+elif numero2 > numero1:
+    print('numero2 é maior que numero1')
 else:
-    print('N2 é igual ao N1')
+    print('numero2 é igual ao numero1')
